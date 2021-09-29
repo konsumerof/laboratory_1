344363de8050149ee221f27864e0de7b99802918
konsumerof <nive54@gmail.com>
2021-09-29 19:52:27 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main() {
+    double rad, dgr;
+    int m,s,gr;
+    cout << "Write amount of degrees = ";
+    cin >> gr;
+    cout << "Write amount of minutes = ";
+    cin >> m;
+    cout << "Write amount of seconds = ";
+    cin >> s;
+    dgr = gr + ((m*60+s)/3600.0);
+    cout << dgr << endl;
+    rad = (M_PI * dgr) / 180;
+    cout << "Amount of radians = " << rad;
+    return 0;
+}
